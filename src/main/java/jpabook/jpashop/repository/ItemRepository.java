695fbecbafef8999aee2bbfc593712c7a740855d
tjdrnr0557 <tjdrnr05571@naver.com>
2021-03-13 23:14:06 +0900

    ddl 설정 변경

--- src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -27,6 +27,7 @@ public class ItemRepository {
 
     public List<Item> findAll() {
         return em.createQuery("select i from Item i", Item.class)
-                .getResultList();
+            .getResultList();
+
     }
 }
--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -23,12 +23,12 @@ public class MemberRepository {
 
     public List<Member> findAll() {
         return em.createQuery("select m from Member m", Member.class)
-                .getResultList();
+            .getResultList();
     }
 
     public List<Member> findByName(String name) {
         return em.createQuery("select m from Member m where m.name = :name", Member.class)
-                .setParameter("name", name)
-                .getResultList();
+            .setParameter("name", name)
+            .getResultList();
     }
 }
